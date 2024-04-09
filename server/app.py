1b3917cc79afe2c66afa61fd216925e45b8be0c8
Felix Omondi <136954351+Felix-svg@users.noreply.github.com>
2024-04-09 21:21:30 +0300

    Complete assignment

--- server/app.py
+++ server/app.py
@@ -7,8 +7,8 @@ from flask_restful import Api, Resource
 from models import db, Plant
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///plants.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = True
 
 migrate = Migrate(app, db)
@@ -16,12 +16,46 @@ db.init_app(app)
 
 api = Api(app)
 
+
 class Plants(Resource):
-    pass
+    def get(self):
+        response_dict_list = [p.to_dict() for p in Plant.query.all()]
+        response = make_response(response_dict_list, 200)
+        return response
+
+    def post(self):
+        new_plant = Plant(
+            name=request.json["name"],
+            image=request.json["image"],
+            price=request.json["price"],
+        )
+        db.session.add(new_plant)
+        db.session.commit()
+        response_dict = new_plant.to_dict()
+        response = make_response(response_dict)
+        return jsonify(response), 201
+
+
+api.add_resource(Plants, "/plants")
+
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        response_dict = Plant.query.filter(Plant.id == id).first().to_dict()
+        response = make_response(response_dict, 200)
+        return response
 
-if __name__ == '__main__':
+    # def delete(self, id):
+    #     plant = Plant.query.filter(Plant.id == id).first()
+    #     db.session.delete(plant)
+    #     db.session.commit()
+    #     response_dict = {"message": f"Plant {id} deleted."}
+    #     response = make_response(response_dict, 200)
+    #     return response
+
+
+api.add_resource(PlantByID, "/plants/<int:id>")
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
